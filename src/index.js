2f2357688d6ce1d1feb18b416f6bafd3c4ae8e36
sand000 <kushwahasandhya68@gmail.com>
2022-09-03 22:42:09 +0530

    done

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const  fs = require("fs");
+
+let app = http.createServer((req,res)=>{
+    if(req.url === "/textsync"){
+        if(req.method === "GET"){ 
+            res.write(fs.readFileSync("./text.txt", {encoding:"utf-8"}))
+            res.end();
+    }
+  }
+    else if(req.url.startsWith("/textasync")){
+            if(req.method === "GET"){
+            res.write(fs.readFile("./example.txt","utf8", function(err,data){
+                console.log(data);
+            }));
+            res.end();
+        }
+     }
+   
+    else if(req.url === "/textstream"){
+        if(req.method === "GET"){
+        let readStream = fs.createReadStream("./stream.txt");
+        res.write(readStream);
+        res.end();
+    }
+ }
+})
+
+app.listen(8070,()=>{
+    console.log("started on : http://localhost:8070")
+})
